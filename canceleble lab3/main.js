19c87320947a882e7f208615c56bdf035086e2c1
SVersj <86892022+SVersj@users.noreply.github.com>
2024-12-28 17:27:54 +0200

    lab3 fix

--- canceleble lab3/main.js
+++ canceleble lab3/main.js
@@ -1,28 +1,29 @@
 async function asyncMapPromiseCancellable(array, promiseFn, { signal }) {
   let results = new Array(array.length);
 
- 
   const tasks = array.map((item, index) => {
     return (async () => {
-     
+      if (signal.aborted) {
+        throw new Error("Operation was cancelled before start");
+      }
+
       const res = await promiseFn(item, signal);
 
-     
+      if (signal.aborted) {
+        throw new Error("Operation was cancelled mid-flight");
+      }
+
       results[index] = res;
     })();
   });
 
-
-  const signalPromise = new Promise((resolve) => {
-    
+  const signalPromise = new Promise((_, reject) => {
     signal.addEventListener("abort", () => {
-      resolve("Signal ignored"); 
+      reject(new Error("Aborted via AbortController"));
     });
   });
 
- 
-  await Promise.race([Promise.all(tasks)]);
+  await Promise.race([Promise.all(tasks), signalPromise]);
 
-  
   return results;
 }
